0e060078c9634fdc5fdbaf777c450b88a02f5a27
ManonLEMAITRE <lemaitremanon5@gmail.com>
2026-08-08 19:19:58 +0200

    Changement d'users pour des rôles plus techniques

--- 0.creation_utilisateurs.py
+++ 0.creation_utilisateurs.py
@@ -41,21 +41,21 @@ def creer_utilisateur_si_absent(db, nom_utilisateur, mot_de_passe, roles):
 
 
 def setup_utilisateurs():
-    """Point d'entrée : crée medecin_user et secretariat_user si nécessaire."""
+    """Point d'entrée : crée medecin_user et analyste_user si nécessaire."""
     client, db = connecter_admin()
 
     try:
         creer_utilisateur_si_absent(
             db,
-            "medecin_user",
-            "medecin123",
+            "pipeline_user",
+            "pipeline123",
             roles=[{"role": "readWrite", "db": "healthcare_db"}],
         )
 
         creer_utilisateur_si_absent(
             db,
-            "secretariat_user",
-            "secretariat123",
+            "analyste_user",
+            "analyste123",
             roles=[{"role": "read", "db": "healthcare_db"}],
         )
 
